ff8f9d98a1dac6e2aed81942a7a7dbcf0f3f7efa
hardtanh <fcdjin@hardtanh.com>
2026-07-09 20:36:01 -0500

    lesson 03

---
+++ lesson03/src/sendwave/utils.py
@@ -0,0 +1,30 @@
+# DocStrings are a way to document your code.
+# They are written in triple quotes and can span multiple lines. 
+# They are used to describe the purpose of a function, class, or module.
+
+def add(a: float, b: float) -> float:
+    """
+    Return the sum of a and b.
+    """
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    """
+    Return the difference of a and b.
+    """
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    """
+    Return the product of a and b.
+    """
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    """
+    Return the quotient of a and b.
+    """
+    return a / b
