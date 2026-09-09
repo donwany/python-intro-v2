cd8c3f95aa979ce8faf373c9e8f4f3b95a24f106
hardtanh <fcdjin@hardtanh.com>
2026-09-08 20:58:17 -0500

    first commit

--- lesson08/errors_handling_v4.py
+++ lesson08/errors_handling_v4.py
@@ -1,5 +1,6 @@
 # put try: except blocks around the code below
 # possibly turn this into a reusable function
+# Reference: https://docs.python.org/3/library/exceptions.html
 
 with open("students.txt", "r") as file:
     contents = file.read()
